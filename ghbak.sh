78e473600d5537ce40e1bb95c1cdcb7d79aa71ed
Phillip Smith <fukawi2@gmail.com>
2011-04-14 12:09:27 +1000

    general improvements

--- ghbak.sh
+++ ghbak.sh
@@ -29,7 +29,8 @@
 
 BACKUP_DIR='test-backups'
 GITHUB_USER='fukawi2'
-API_FORMAT='yaml'
+GITHUB_TOKEN=''	# Obtain your key from 'Account Settings' => 'Account Admin'
+API_FORMAT='yaml'	# Do Not Change This
 
 ########################
 # FUNCTIONS
@@ -50,32 +51,44 @@ function bomb() {
 ########################
 # MAIN
 ########################
+
+# Sanity Checks
 [[ ! -d "$BACKUP_DIR" ]] && { mkdir -p $BACKUP_DIR || exit 1; }
 absolute_backup_dir=$(cd $BACKUP_DIR && pwd)
 [[ -z "$absolute_backup_dir" ]] && { echo "Failed to obtain absolute path for '$BACKUP_DIR'"; exit 1; }
+[[ -z "$GITHUB_USER" ]]	&& bomb 'GitHub username not configured'
+[[ -z "$GITHUB_TOKEN" ]]	&& warn 'User API Token not configured; Private Repositories will NOT be backed up'
 
-repos=$(curl --silent -i http://github.com/api/v2/$API_FORMAT/repos/show/${GITHUB_USER} | grep -F ':name:')
+# Fetch a list of repos for $GITHUB_USER
+repos=$(curl --silent -i -u "${GITHUB_USER}/token:${GITHUB_TOKEN}" \
+	https://github.com/api/v2/$API_FORMAT/repos/show/${GITHUB_USER} | grep -F ':name:' | awk '{ print $2 }')
 
 # TODO: Handle multiple pages of repos:
 # X-Next: http://github.com/api/v2/yaml/repos/show/schacon?page=2
 # X-Last: http://github.com/api/v2/yaml/repos/show/schacon?page=3
 
-for r in $repos ; do
-	repo_name=${r##*:}
+echo "Backup location is '$absolute_backup_dir/'"
+echo "Found $(wc -w <<< $repos) repositories to backup"
+
+# Loop through the found repos and 'clone' or 'fetch' as appropriate
+#for r in $repos ; do
+for repo_name in $repos ; do
 	[[ -z "$repo_name" ]] && continue
 
-	clone_name="$repo_name"
-	[[ ${repo_name:0:1} == '.' ]] && clone_name="${repo_name:1}"
+	# We don't want backups in dotdirs (hidden)
+	[[ ${repo_name:0:1} == '.' ]] && clone_name="${repo_name:1}" || clone_name="$repo_name"
 	
 	cd $absolute_backup_dir || exit 1
 	if [[ ! -d "${clone_name}" ]] ; then
 		# New Repo; Clone it
-		echo "===> Found new repository '$repo_name'; Cloning to $absolute_backup_dir/$clone_name"
-		git clone --mirror --quiet git://github.com/${GITHUB_USER}/${repo_name}.git $clone_name
+		echo "===> Found new repository '$repo_name'; Cloning to $clone_name/"
+		git clone --mirror --quiet git://github.com/${GITHUB_USER}/${repo_name}.git $clone_name/
+		[[ $? -ne 0 ]] && warn "Failed to clone '$repo_name' to '${clone_name}/'"
 	else
-		# Existing; Pull Update
+		# Existing; Fetch changes
 		echo "===> Fetching changes in existing backup for '$repo_name'"
-		cd ${clone_name}.git && git fetch --quiet --all --prune
+		cd ${clone_name} && git fetch --quiet --all --prune
+		[[ $? -ne 0 ]] && warn "Failed to fetch changes in '$repo_name' (Path: ${clone_name}/)"
 	fi
 done
 
